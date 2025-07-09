ed0ecbe4bad8f6889645cce2640be32221a02f3a
Sharmi-v <sharmivictor12@gmail.com>
2025-07-09 12:05:55 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+int sum=a+b;
+int subract=a-b;
+int product=a*b;
+int quotient=a/b;
+System.out.println("Sum of a and b is"+sum);
+System.out.println("Subtraction of a and b is "+subtract);
+System.out.println("Multiplication of a and b is"+product);
+System.out.println("Division of a and b is"+quotient);
